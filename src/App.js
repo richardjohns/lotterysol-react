36192ad7bf4ef76890dbc262bdb4582a12e2dc0e
richardjohns <richard.johns193@gmail.com>
2018-10-21 00:28:18 +1300

    Complete Lottery.js contract link file

--- src/App.js
+++ src/App.js
@@ -2,11 +2,28 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import web3 from './web3';
+// import Lottery from './Lottery';
 
 class App extends Component {
+  state = {
+    manager: ''
+  };
+  // constructor(props) {
+  //   super(props)
+
+  //   this.state = { manager: '' };
+  // }
+
+  componentDidMount() {
+    // don't need to add 'from: account[0]' as provider already has the default account added.
+    // const manager = await Lottery.methods.manager().call()
+
+    // this.setState({ manager })
+  }
+
   render() {
-    console.log('Using web3 version: ',web3.version)
-    web3.eth.getAccounts().then(console.log)
+    console.log('Using web3 version: ',web3.version);
+    web3.eth.getAccounts().then(console.log);
 
     return (
       <div className="App">
@@ -15,15 +32,12 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
+        <div>
+          <h2>Lottery Contract</h2>
+          <p>The contract is managed by: {this.state.manager}</p>
+        </div>
+
       </div>
     );
   }
--- src/Lottery.js
+++ src/Lottery.js
@@ -1,9 +1,86 @@
 import web3 from './web3'
 
-const address = 'rinkyaddressfromdeployedLotterysolproject'
+const address = '0xa932EEb44887b67E1645A53CA2F91d6662A7F8f9'
 
-const abi =
-[{ interfaceCodeFromLotterysol: [] 
+const abi = [{
+    "constant": true,
+    "inputs": [],
+    "name": "manager",
+    "outputs": [{
+        "name": "",
+        "type": "address"
+    }],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+}, {
+    "constant": false,
+    "inputs": [],
+    "name": "pickWinner",
+    "outputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+}, {
+    "constant": true,
+    "inputs": [],
+    "name": "getPlayers",
+    "outputs": [{
+        "name": "",
+        "type": "address[]"
+    }],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+}, {
+    "constant": true,
+    "inputs": [{
+        "name": "",
+        "type": "uint256"
+    }],
+    "name": "winners",
+    "outputs": [{
+        "name": "",
+        "type": "address"
+    }],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+}, {
+    "constant": false,
+    "inputs": [],
+    "name": "resetLottery",
+    "outputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+}, {
+    "constant": false,
+    "inputs": [],
+    "name": "enter",
+    "outputs": [],
+    "payable": true,
+    "stateMutability": "payable",
+    "type": "function"
+}, {
+    "constant": true,
+    "inputs": [{
+        "name": "",
+        "type": "uint256"
+    }],
+    "name": "players",
+    "outputs": [{
+        "name": "",
+        "type": "address"
+    }],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+}, {
+    "inputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "constructor"
 }]
 
 export default new web3.eth.Contract(abi, address);
